fbc4d89490598478bc573ae2307cf2b8475635bc
4lex11 <95388653+4lex11@users.noreply.github.com>
2024-11-07 17:33:39 -0500

    Update index.js

--- ProyectoWiksa/js/index.js
+++ ProyectoWiksa/js/index.js
@@ -1,49 +1,15 @@
-/*const contenerdorTarjetas = document.getElementById("supermercados-container");
-const contenedorMenuWong = document.getElementById("productos-wong-container");
-const contenedorSupermercados = document.getElementById("lista-restaurantes-containter");
+/*const contenerdorProductos = document.getElementById("productos-container");
+const modal = document.getElementById("platoModal");
 
-function crearTarjetasupermercados(supermercados){
-    supermercados.forEach(supermercado => {
-        const nuevoSuperMercado = document.createElement("div");
-        nuevoSuperMercado.classList = "logo_supermcado";
-        nuevoSuperMercado.innerHTML = `
-        <a href="../pages/supermercados/${supermercado.linkUrl}">
-            <img src="../img/iconos/${supermercado.imgUrl}" />
-            <span>${supermercado.name}</span>
-        </a>
-        `;
-        contenerdorTarjetas.appendChild(nuevoSuperMercado);
-    });
-};
+const openModal = () => {
+    modal.showModal();
+}
 
-function crearMenuWong(prodWong){
-    prodWong.forEach(producto => {
-          const nuevoProdWong = document.createElement("div");
-          nuevoProdWong.classList = "item_menu";
-          nuevoProdWong.innerHTML = `
-              <span>${producto.name}</span>
-          `;
-          contenedorMenuWong.appendChild(nuevoProdWong);
-      });
-};
+const closeModal = () => {
+    modal.close();
+}
 
-function ListadoSupermercados(supermercados){
-    supermercados.forEach(supermercado => {
-          const nuevoSupermercadolst = document.createElement("div");
-          nuevoSupermercadolst.classList = "item_menu";
-          nuevoSupermercadolst.innerHTML = `
-            <a href="../pages/supermercados/${supermercado.linkUrl}" >
-                <span>${supermercado.name}</span>
-            </a>
-          `;
-          contenedorSupermercados.appendChild(nuevoSupermercadolst);
-      });
-};
 
-crearTarjetasupermercados(supermercados);
-ListadoSupermercados(supermercados);
-crearMenuWong(prodWong);*/
-const contenerdorProductos = document.getElementById("productos-container");
 function crearTarjetasProductos(productos){
     productos.forEach(producto => {
         const nuevoProducto = document.createElement("div");
@@ -55,6 +21,69 @@ function crearTarjetasProductos(productos){
                 <span>Precio: ${producto.price}</span>
                 <span>Tiempo de entrega: ${producto.delivery_time}</span>
                 <span>Precio de entrega: ${producto.delivery_price}</span>
+                <button onclick="openModal(${JSON.stringify(producto)})">Agregar al carro</button>
+            </div>
+            <dialog class="modal" id="platoModal">
+                    <button onclick="closeModal()" class="btn-close-modal">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                          </svg>
+                    </button>
+                        <img src="https://pedidosya.dhmedia.io/image/pedidosya/products/af244dc5-7566-4591-ae8e-7c0f1540b0be.jpg?quality=90&width=768">
+                    <div class="modal-info">  
+                        <span>Plato 1</span>  
+                        <p>Descripcion</p>
+                        <input type="number">
+                        <button class="btnagrpedido">Agregar al pedido</button>
+                    </div>
+
+            </dialog>
+        `;
+        contenerdorProductos.append(nuevoProducto);
+    });
+};
+
+crearTarjetasProductos(productos);*/
+const contenerdorProductos = document.getElementById("productos-container");
+const modal = document.getElementById("platoModal");
+const modalContent = document.getElementById("modalContent");
+
+const openModal = (producto) => {
+    modalContent.innerHTML = `
+        <button onclick="closeModal()" class="btn-close-modal">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+            </svg>
+        </button>
+        <img src="../../img/${producto.imgUrl}">
+        <div class="modal-info">
+            <span>${producto.name}</span>
+            <p>Descripción: ${producto.description}</p>
+            <input type="number" placeholder="Cantidad">
+            <button class="btnagrpedido">Agregar al pedido</button>
+        </div>
+    `;
+    nuevoModal.append(dialog);
+    modal.showModal();
+};
+
+const closeModal = () => {
+    modal.close();
+};
+
+
+function crearTarjetasProductos(productos){
+    productos.forEach(producto => {
+        const nuevoProducto = document.createElement("div");
+        nuevoProducto.classList = "menu-plato";
+        nuevoProducto.innerHTML = `
+            <img src="../../img/${producto.imgUrl}">
+            <div class="menu-des">
+                <span>${producto.name}</span>
+                <span>Precio: ${producto.price}</span>
+                <span>Tiempo de entrega: ${producto.delivery_time}</span>
+                <span>Precio de entrega: ${producto.delivery_price}</span>
+                <button onclick="openModal(${JSON.stringify(producto)})">Agregar al carro</button>
             </div>
         `;
         contenerdorProductos.append(nuevoProducto);
@@ -63,6 +92,23 @@ function crearTarjetasProductos(productos){
 
 crearTarjetasProductos(productos);
 
-$(document).bind('keydown', 'ctrl+q', function(){
-    alert("Activado modo para....");
-  });
+
+/*
+function crearTarjetasProductos(productos) {
+    productos.forEach(producto => {
+        const nuevoProducto = document.createElement("div");
+        nuevoProducto.classList = "menu-plato";
+        nuevoProducto.innerHTML = `
+            <img src="../../img/${producto.imgUrl}">
+            <div class="menu-des">
+                <span>${producto.name}</span>
+                <span>Precio: ${producto.price}</span>
+                <span>Tiempo de entrega: ${producto.delivery_time}</span>
+                <span>Precio de entrega: ${producto.delivery_price}</span>
+                <button onclick="openModal(${JSON.stringify(producto)})">Agregar al carro</button>
+            </div>
+        `;
+        contenerdorProductos.append(nuevoProducto);
+    });
+};
+*/
