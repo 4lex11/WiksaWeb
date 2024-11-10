d994e3e87820a3438050d1e4398439255561c45e
4lex11 <95388653+4lex11@users.noreply.github.com>
2024-11-09 20:26:25 -0500

    Update index.js

--- ProyectoWiksa/js/index.js
+++ ProyectoWiksa/js/index.js
@@ -1,25 +1,3 @@
-/*const contenerdorProductos = document.getElementById("productos-container");
-
-function CargarProductos(productos){
-    productos.forEach(producto => {
-        const nuevoProducto = document.createElement("div");
-        nuevoProducto.classList = "menu-plato";
-        nuevoProducto.innerHTML = `
-            <img src="../../img/${producto.imgUrl}">
-            <div class="menu-des">
-                <span>${producto.name}</span>
-                <span>Precio: ${producto.price}</span>
-                <span>Tiempo de entrega: ${producto.delivery_time}</span>
-                <span>Precio de entrega: ${producto.delivery_price}</span>
-                <button class="abrirmodal" id="${producto.id}">Agregar al carro</button>
-            </div>
-        `;
-        contenerdorProductos.append(nuevoProducto);
-    });
-};
-
-CargarProductos(productos);*/
-
 document.addEventListener("DOMContentLoaded", () => {
     // Selecciona todos los botones y enlaces que quieras hacer accesibles
     const elements = document.querySelectorAll("a, button");
@@ -42,4 +20,24 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   });
-  
+
+const contenerdorProductos = document.getElementById("productos-container");
+function CargarProductos(productos){
+    productos.forEach(producto => {
+        const nuevoProducto = document.createElement("div");
+        nuevoProducto.classList = "menu-plato";
+        nuevoProducto.innerHTML = `
+            <img src="../../img/${producto.imgUrl}">
+            <div class="menu-des">
+                <span>${producto.name}</span>
+                <span>Precio: ${producto.price}</span>
+                <span>Tiempo de entrega: ${producto.delivery_time}</span>
+                <span>Precio de entrega: ${producto.delivery_price}</span>
+                <button class="abrirmodal" id="${producto.id}">Agregar al carro</button>
+            </div>
+        `;
+        contenerdorProductos.append(nuevoProducto);
+    });
+};
+
+CargarProductos(productos);
