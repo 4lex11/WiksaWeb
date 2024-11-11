9288f5d0e795c465e0cfbf734f45cebe32c68410
4lex11 <95388653+4lex11@users.noreply.github.com>
2024-11-10 20:06:38 -0500

    Update carrito.js

--- ProyectoWiksa/js/carrito.js
+++ ProyectoWiksa/js/carrito.js
@@ -1,2 +1,11 @@
 const productosEnCarrito = JSON.parse(localStorage.getItem("productos_en_carro"));
 console.log(productosEnCarrito);
+
+const carritovacio = document.querySelector
+
+if(productosEnCarrito){
+
+}
+else{
+
+}
