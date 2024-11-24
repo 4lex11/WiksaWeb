b1478a82f006185fb7c7b58bbe2c2d5a4f284f60
RogelioMMI <rogelio_c.r@hotmail.com>
2024-11-24 15:16:28 -0500

    Update carrito.js

--- ProyectoWiksa/js/carrito.js
+++ ProyectoWiksa/js/carrito.js
@@ -25,12 +25,12 @@ function cargarProductosCarrito(){
             const div = document.createElement("div");
             div.classList.add("carrito_item");
             div.innerHTML= `
-                <span>${producto.name}</span>
-                <span>Cant: ${producto.cantidad}</span>
-                <span>Precio: S/${producto.price}</span>
+                <span tabindex="0">${producto.name}</span>
+                <span tabindex="0">Cantidad: ${producto.cantidad}</span>
+                <span tabindex="0">Precio: ${producto.price} soles</span>
                 <div>
-                    <span>Subtotal: S/ ${producto.price *producto.cantidad}</span>
-                    <button id="${producto.id}" class="eliminar_producto"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                    <span tabindex="0">Subtotal: ${producto.price *producto.cantidad} soles</span>
+                    <button id="${producto.id}" class="eliminar_producto" aria-label="Eliminar Item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                     <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                     </svg>
@@ -79,7 +79,7 @@ function actualizarTotal(){
     const totalcalculado = productosEnCarrito.reduce((acc, producto) => acc + (producto.price * producto.cantidad), 0).toFixed(2);
     contentTotal.innerHTML = `S/ ${totalcalculado}`;
     const montoTotal = (parseFloat(totalcalculado) + 5.00).toFixed(2);
-    contentMontoTotal.innerHTML = `Monto Total: S/ ${montoTotal}`;
+    contentMontoTotal.innerHTML = `Monto Total: ${montoTotal} soles`;
 }
 
 botonpedir.addEventListener("click",comprarCarrito)
